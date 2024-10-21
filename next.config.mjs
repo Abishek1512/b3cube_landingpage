d2b4732df04b806bff3a2165054679e0ac05cb4f
Yogapriya <yoga@francium.tech>
2024-10-21 18:26:23 +0530

    Added team slider section

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            '/',
-            '*',
-            'cdn.prod.website-files.com',
-        ]
-    },
+        domains: ['cdn.prod.website-files.com'], // Add the domain here
+      },
 };
 
 export default nextConfig;
