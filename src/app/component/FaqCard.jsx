e9c230dbbdde36f8692524e830885b36d2840341
SunilFrancium <sunil@francium.tech>
2024-10-22 13:08:17 +0530

    Faqs intial commit

---
+++ src/app/component/FaqCard.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import PropTypes from "prop-types";
+
+function FaqCard({title, content, index, isOpen, toggleAccordion, styles}) {
+  return (
+    <div className={`mb-2 transition-all duration-500 overflow-hidden`}>
+      {/* Accordion Header */}
+      <div className={`${styles.faqbg}`}>
+        <div className="flex flex-col transition-all duration-500">
+          <div className="flex justify-between items-center z-10 text-lg font-bold p-6 relative cursor-pointer w-full" onClick={() => toggleAccordion(index)}>
+            <h2 className="text-white w-10/12">{title}</h2>
+            <span className="rounded-[6px] flex justify-center items-center w-[30px] h-[30px] transition-all duration-500">
+              {isOpen ? (
+                <div className="rounded-[6px] bg-white bg-opacity-20 w-full h-full flex justify-center items-center transition-all duration-500 opacity-100">
+                  <div className="bg-white w-3 h-[1.5px]"></div>
+                  <div className="bg-white w-3 h-[1.5px] absolute transform transition-transform duration-500 rotate-180"></div>
+                </div>
+              ) : (
+                <div className="transition-all duration-500 opacity-100 bg-white bg-opacity-0 w-full h-full flex justify-center items-center">
+                  <div className="bg-white w-3 h-[1.5px]"></div>
+                  <div className="bg-white w-3 h-[1.5px] absolute transform transition-transform duration-500 rotate-90"></div>
+                </div>
+              )}
+            </span>
+          </div>
+
+          {/* Accordion Content */}
+          <div
+            className={`text-[#b7b8be] text-lg mb-0 leading-[1.6] transition-all duration-500 ${
+              isOpen ? "opacity-100 max-h-[500px]" : "opacity-0 max-h-0"
+            }`}
+          >
+            <p
+              className="pb-6 pl-6 pr-6"
+              dangerouslySetInnerHTML={{__html: content}}
+            ></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Define PropTypes for the component to ensure the correct data is passed
+FaqCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  toggleAccordion: PropTypes.func.isRequired,
+};
+
+export default FaqCard;
--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,14 @@ import HeroSection from "./herobanner/page";
 import Solutions from '../components/LandingPage/Solutions'
 
 import TokenInroduction from "../components/IntroToken/Page";
+import Faqs from '../components/LandingPage/Faqs'
 function Home() {
   return (
     <>
-    {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"> */}
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"> */}
+      <section className="container mx-auto">
+        <Faqs />
+      </section>
       <HeroSection />
       <div className="my-8">
         <Features />
@@ -25,4 +29,4 @@ function Home() {
   );
 }
 
-export default  Home;
+export default Home;
