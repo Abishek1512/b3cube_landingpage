3b5a6fa9cf619a3c2d93aeac2b0d22f8dfeaa374
SunilFrancium <sunil@francium.tech>
2024-10-22 13:09:27 +0530

    Faq order change

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,6 @@ function Home() {
   return (
     <>
       {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"> */}
-      <section className="container mx-auto">
-        <Faqs />
-      </section>
       <HeroSection />
       <div className="my-8">
         <Features />
@@ -19,6 +16,9 @@ function Home() {
       <div>
         <Benefits />
       </div>
+      <section className="container mx-auto">
+        <Faqs />
+      </section>
       {/* </div> */}
     </>
   );
